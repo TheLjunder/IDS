01261c8a9d3936263974a0b4cb0b036aa1e2edec
Domagoj <domagojdomy3@gmail.com>
2023-08-17 16:03:07 +0200

    app v1 done gnb implemented

--- IDS.py
+++ IDS.py
@@ -1,6 +1,4 @@
-print("=" * 30)
-print("| Sustav za oktrivanje upada |")
-print("=" * 30)
+# Uvoz potrebnih biblioteka
 import os
 import pandas as pd
 import numpy as np
@@ -8,10 +6,21 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import time
 from colorama import Fore, Style
-
-print(Fore.YELLOW + "Učitavam program." + Style.RESET_ALL)
-# Dohvaćanje putanje mape u kojoj se nalazi trenutna datoteka.
+from sklearn.metrics import confusion_matrix, precision_score, accuracy_score
+from sklearn.preprocessing import MinMaxScaler
 from pathlib import Path
+from sklearn.naive_bayes import GaussianNB
+
+#Globalne varijable
+AttackCategoryList = []
+
+# Početak programa
+print("=" * 30)
+print(Fore.GREEN + "| Sustav za oktrivanje upada |" + Style.RESET_ALL)
+print("=" * 30)
+print(Fore.YELLOW + "Učitavam program." + Style.RESET_ALL)
+
+# Dohvaćanje putanje mape u kojoj se nalazi trenutna datoteka.
 currentPath = Path(__file__).parent.absolute()
 
 # Postavljanje putanje za skup podataka kojim će se trenirati model za detekciju.
@@ -62,6 +71,49 @@ attacksNamesAndCategory = {
 'warezmaster': 'r2l',
 }
 
+# Izrada funkcije koja će raditi s modelima 
+def obradiModel(trainDF_Y, trainDF_X, testDF_Y, testDF_X, model, vrstaModela):
+    print(Fore.YELLOW + "Započinjem obradu modela." + Style.RESET_ALL)
+    # Treniranje modela
+    print(Fore.YELLOW + "Započinjem treniranje modela." + Style.RESET_ALL)
+    startTime = time.time()
+    model.fit(trainDF_X, trainDF_Y.values.ravel())
+    endTime = time.time()
+    vrijemeTreniranja = endTime-startTime
+    print(Fore.YELLOW + "Treniranje modela završeno." + Style.RESET_ALL)
+    
+    # Testiranje modela
+    print(Fore.YELLOW + "Započinjem testiranje modela." + Style.RESET_ALL)
+    startTime = time.time()
+    prediction = model.predict(testDF_X)
+    endTime = time.time()
+    vrijemeTestiranja = endTime-startTime
+    print(Fore.YELLOW + "Testiranje modela završeno." + Style.RESET_ALL)
+
+    # Izračunavanje statistike
+    print(Fore.YELLOW + "Izračunavam statističke podatke modela." + Style.RESET_ALL)
+    confusionMatrix = confusion_matrix(testDF_Y, prediction)
+    truePositive = np.diag(confusionMatrix)
+    falsePositive = np.sum(confusionMatrix, axis=0) - truePositive
+    falseNegative = np.sum(confusionMatrix, axis=1) - truePositive
+    trueNegative = np.sum(confusionMatrix) - np.sum([truePositive, falsePositive, falseNegative], axis = 0)
+    precision = precision_score(testDF_Y, prediction, average = 'weighted')
+    accuracy = accuracy_score(testDF_Y, prediction)
+
+    print()
+    print(Fore.GREEN + "Statistika modela " + vrstaModela + Style.RESET_ALL) 
+    print("Vrijeme treniranja: ",vrijemeTreniranja)
+    print("Vrijeme testiranja: ",vrijemeTestiranja)
+    print('True Positive po kategoriji: ' + str(truePositive))
+    print('False Positive po kategoriji: ' + str(falsePositive))
+    print('False Negative po kategoriji: ' + str(falseNegative))
+    print('True Negative po kategoriji: ' + str(trueNegative))
+    print("Preciznost modela: " + str(precision))
+    print("Točnost modela: " + str(accuracy))
+    print()
+
+    print(Fore.YELLOW + "Obrada modela završena." + Style.RESET_ALL)
+
 # Učitavanje početnog skupa podataka za treniranje.
 print(Fore.YELLOW + "Učitavam skup podataka za treniranje." + Style.RESET_ALL)
 trainDF = pd.read_csv(trainingDataPath, names = columnHeaderNames)
@@ -74,10 +126,11 @@ testDF = pd.read_csv(testingDataPath, names = columnHeaderNames)
 print(Fore.YELLOW + "Dodajem kategorije napada u skupove podataka." + Style.RESET_ALL)
 trainDF['attack_category'] = trainDF.attack_name.apply(lambda r:attacksNamesAndCategory[r[:-1]])
 testDF['attack_category'] = testDF.attack_name.apply(lambda r:attacksNamesAndCategory[r[:-1]])
+
+# Izbroji kategorije napada
 countAttackCategoryTestDF = testDF['attack_category'].value_counts()
-print(countAttackCategoryTestDF)
-attackCategoryList = countAttackCategoryTestDF.index.tolist()
-print(attackCategoryList)
+# Dohvati raspored kategorija napada
+AttackCategoryList = countAttackCategoryTestDF.index.tolist()
 
 # Pretvorimo simboličke pokazatelje u numeričke.
 print(Fore.YELLOW + "Pretvaram simboličke pokazatelje u numeričke." + Style.RESET_ALL)
@@ -94,65 +147,59 @@ print(Fore.YELLOW + "Uklanjam service simbolički pokazatelj." + Style.RESET_ALL
 trainDF.drop('service',axis = 1,inplace= True)
 testDF.drop('service',axis = 1,inplace= True)
 
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
 
+# Uklonimo simboličke stupce iz skupa podataka
 print(Fore.YELLOW + "Oblikujem skupove podataka za unos u model." + Style.RESET_ALL)
 trainDF = trainDF.drop(['attack_name',], axis=1)
 testDF = testDF.drop(['attack_name',], axis=1)
 
-# Target variable and train set
+# Postavimo varijable za treniranje i testiranje
 trainDF_Y = trainDF[['attack_category']]
 trainDF_X = trainDF.drop(['attack_category',], axis=1)
 testDF_Y = testDF[['attack_category']]
 testDF_X = testDF.drop(['attack_category',], axis=1)
 
+# Normaliziramo skup podataka po MinMax skali kako bi 
+# model bolje funkcionirao
 print(Fore.YELLOW + "Normaliziram skupove podataka." + Style.RESET_ALL)
 sc = MinMaxScaler()
 trainDF_X = sc.fit_transform(trainDF_X)
 testDF_X = sc.fit_transform(testDF_X)
 
-from sklearn.naive_bayes import GaussianNB
+# Napravimo instance modela za različite
+# metode strojnog učenja
+print(Fore.YELLOW + "Kreiram instancu modela za GNB metodu." + Style.RESET_ALL)
+modelGNB = GaussianNB()
 
-model1 = GaussianNB()
+# Unos parametara u pojednini model pomoću
+# metode koja ujedno vraća i statističke podatke
+obradiModel(trainDF_Y, trainDF_X, testDF_Y, testDF_X, modelGNB, "Gaussian Naive Bayes")
 
-print(Fore.YELLOW + "Započinjem treniranje modela." + Style.RESET_ALL)
-start_time = time.time()
-model1.fit(trainDF_X, trainDF_Y.values.ravel())
-end_time = time.time()
 
-print("Training time: ",end_time-start_time)
 
-print(Fore.YELLOW + "Započinjem testiranje modela." + Style.RESET_ALL)
-start_time = time.time()
-Y_test_pred1 = model1.predict(testDF_X)
-end_time = time.time()
+# # Template code
 
-print("Testing time: ",end_time-start_time)
+# # Izračunavanje statistike
 
-# Izračunavanje statistike
+# # from sklearn.metrics import confusion_matrix
 
-from sklearn.metrics import confusion_matrix
+# # confusionMatrix = confusion_matrix(testDF_Y, Y_test_pred1)
+# # confusionMatrixDF = pd.DataFrame(confusionMatrix, index = ['normal', 'dos', 'probe', 'r2l', 'u2r'], columns = ['normal', 'dos', 'probe', 'r2l', 'u2r'])
+# # plt.figure(figsize=(15,10))
+# # plt.title('Confusion Matrix for Gaussian Naives Bayes Classifier on KDD-NSL Dataset')
+# # plt.ylabel('Actual Values')
+# # plt.xlabel('Predicted Values')
+# # sns.heatmap(confusionMatrixDF, annot=True, annot_kws={"size": 10}) # font size
+# # plt.show(block=False)
 
-confusionMatrix = confusion_matrix(testDF_Y, Y_test_pred1)
-confusionMatrixDF = pd.DataFrame(confusionMatrix, index = ['normal', 'dos', 'probe', 'r2l', 'u2r'], columns = ['normal', 'dos', 'probe', 'r2l', 'u2r'])
-plt.figure(figsize=(15,10))
-plt.title('Confusion Matrix for Gaussian Naives Bayes Classifier on KDD-NSL Dataset')
-plt.ylabel('Actual Values')
-plt.xlabel('Predicted Values')
-sns.heatmap(confusionMatrixDF, annot=True, annot_kws={"size": 10}) # font size
-plt.show(block=False)
 
-tp = np.diag(confusionMatrix)
-tp = pd.Series(tp)
+# # tp = np.diag(confusionMatrix)
+# # tp = pd.Series(tp)
+# # tp = tp.set_axis(AttackCategoryList)
+# # diagramDF = pd.concat([countAttackCategoryTestDF, tp], axis = 1)
+# # diagramDF.columns = ['Test', 'Predict']
+# # diagramDF.plot(y = ["Test", "Predict"], kind = "bar")
+# # plt.show()
 
-tp = tp.set_axis(attackCategoryList)
-
-diagramDF = pd.concat([countAttackCategoryTestDF, tp], axis = 1)
-diagramDF.columns = ['Test', 'Predict']
-
-diagramDF.plot(y = ["Test", "Predict"], kind = "bar")
-plt.show()
-
-print("Train score is:", model1.score(trainDF_X, trainDF_Y))
-print("Test score is:",model1.score(testDF_X, testDF_Y))
+# # print("Train score is:", modelGNB.score(trainDF_X, trainDF_Y))
+# # print("Test score is:",modelGNB.score(testDF_X, testDF_Y))
